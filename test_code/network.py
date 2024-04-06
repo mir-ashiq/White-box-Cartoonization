51acd81a9895ff669756beffb8cfae9d697431ba
Ashiq Hussain Mir <128612922+mir-ashiq@users.noreply.github.com>
2024-04-06 06:29:27 +0530

    Update network.py

--- test_code/network.py
+++ test_code/network.py
@@ -1,6 +1,5 @@
 import tensorflow as tf
 import numpy as np
-import tensorflow.contrib.slim as slim
 
 
 
@@ -8,11 +7,9 @@ def resblock(inputs, out_channel=32, name='resblock'):
     
     with tf.variable_scope(name):
         
-        x = slim.convolution2d(inputs, out_channel, [3, 3], 
-                               activation_fn=None, scope='conv1')
-        x = tf.nn.leaky_relu(x)
-        x = slim.convolution2d(x, out_channel, [3, 3], 
-                               activation_fn=None, scope='conv2')
+        x = tf.keras.layers.Conv2D(out_channel, (3, 3), activation=None, name='conv1')(inputs)
+        x = tf.nn.relu(x)
+        x = tf.keras.layers.Conv2D(out_channel, (3, 3), activation=None, name='conv2')(x)
         
         return x + inputs
 
@@ -22,37 +19,37 @@ def resblock(inputs, out_channel=32, name='resblock'):
 def unet_generator(inputs, channel=32, num_blocks=4, name='generator', reuse=False):
     with tf.variable_scope(name, reuse=reuse):
         
-        x0 = slim.convolution2d(inputs, channel, [7, 7], activation_fn=None)
-        x0 = tf.nn.leaky_relu(x0)
+        x0 = tf.keras.layers.Conv2D(channel, (7, 7), activation=None)(inputs)
+        x0 = tf.nn.relu(x0)
         
-        x1 = slim.convolution2d(x0, channel, [3, 3], stride=2, activation_fn=None)
-        x1 = tf.nn.leaky_relu(x1)
-        x1 = slim.convolution2d(x1, channel*2, [3, 3], activation_fn=None)
-        x1 = tf.nn.leaky_relu(x1)
+        x1 = tf.keras.layers.Conv2D(channel, (3, 3), strides=2, activation=None)(x0)
+        x1 = tf.nn.relu(x1)
+        x1 = tf.keras.layers.Conv2D(channel*2, (3, 3), activation=None)(x1)
+        x1 = tf.nn.relu(x1)
         
-        x2 = slim.convolution2d(x1, channel*2, [3, 3], stride=2, activation_fn=None)
-        x2 = tf.nn.leaky_relu(x2)
-        x2 = slim.convolution2d(x2, channel*4, [3, 3], activation_fn=None)
-        x2 = tf.nn.leaky_relu(x2)
+        x2 = tf.keras.layers.Conv2D(channel*2, (3, 3), strides=2, activation=None)(x1)
+        x2 = tf.nn.relu(x2)
+        x2 = tf.keras.layers.Conv2D(channel*4, (3, 3), activation=None)(x2)
+        x2 = tf.nn.relu(x2)
         
         for idx in range(num_blocks):
             x2 = resblock(x2, out_channel=channel*4, name='block_{}'.format(idx))
             
-        x2 = slim.convolution2d(x2, channel*2, [3, 3], activation_fn=None)
-        x2 = tf.nn.leaky_relu(x2)
+        x2 = tf.keras.layers.Conv2D(channel*2, (3, 3), activation=None)(x2)
+        x2 = tf.nn.relu(x2)
         
         h1, w1 = tf.shape(x2)[1], tf.shape(x2)[2]
-        x3 = tf.image.resize_bilinear(x2, (h1*2, w1*2))
-        x3 = slim.convolution2d(x3+x1, channel*2, [3, 3], activation_fn=None)
-        x3 = tf.nn.leaky_relu(x3)
-        x3 = slim.convolution2d(x3, channel, [3, 3], activation_fn=None)
-        x3 = tf.nn.leaky_relu(x3)
+        x3 = tf.image.resize(x2, (h1*2, w1*2))
+        x3 = tf.keras.layers.Conv2D(channel*2, (3, 3), activation=None)(x3+x1)
+        x3 = tf.nn.relu(x3)
+        x3 = tf.keras.layers.Conv2D(channel, (3, 3), activation=None)(x3)
+        x3 = tf.nn.relu(x3)
 
         h2, w2 = tf.shape(x3)[1], tf.shape(x3)[2]
-        x4 = tf.image.resize_bilinear(x3, (h2*2, w2*2))
-        x4 = slim.convolution2d(x4+x0, channel, [3, 3], activation_fn=None)
-        x4 = tf.nn.leaky_relu(x4)
-        x4 = slim.convolution2d(x4, 3, [7, 7], activation_fn=None)
+        x4 = tf.image.resize(x3, (h2*2, w2*2))
+        x4 = tf.keras.layers.Conv2D(channel, (3, 3), activation=None)(x4+x0)
+        x4 = tf.nn.relu(x4)
+        x4 = tf.keras.layers.Conv2D(3, (7, 7), activation=None)(x4)
         
         return x4
 
